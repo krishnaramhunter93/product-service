8d1d8eab5182cee4bc69853ef847fa3a58cf6b1d
krishnaramhunter93 <140825570+krishnaramhunter93@users.noreply.github.com>
2025-06-15 07:50:16 -0400

    Fix typo in method name

--- src/main/java/com/microservice/productservice/controller/ProductController.java
+++ src/main/java/com/microservice/productservice/controller/ProductController.java
@@ -128,7 +128,7 @@ public class ProductController {
 	@PostMapping(value = "/savelist")
 	public ResponseEntity<?> saveListOfProducts(@RequestBody List<ProductRequest> productRequests) {
 		logger.info("saveListOfProducts API has started");
-		List<Product> productList = productService.createListOfProdcuts(productRequests);
+               List<Product> productList = productService.createListOfProducts(productRequests);
 		return ResponseEntity.ok().body(productList);
 	}
 }
